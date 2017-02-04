efc6edd36b281e3c5332c25b5ed5f5e4de3b7b1a
andy6804tw <andy6804tw@yahoo.com.tw>
2017-02-04 22:35:49 +0800

    第一個CardView 日期、時間、位置、溫度、最高低溫顯示

--- app/src/main/java/com/openweather/openweather/WeatherNow/WeatherNowRVA.java
+++ app/src/main/java/com/openweather/openweather/WeatherNow/WeatherNowRVA.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +16,7 @@ import com.openweather.openweather.R;
  * Created by andy6804tw on 2017/1/25.
  */
 
-public class WeatherNowRVA extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class WeatherNowRVA extends RecyclerView.Adapter<WeatherNowRVA.ViewHolder> {
 
     private final Context context;
     private static final int ITEM_COUNT = 10;
@@ -30,30 +31,62 @@ public class WeatherNowRVA extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
 
     class ViewHolder extends RecyclerView.ViewHolder{
-
-
+        //第零個
+        public TextView tv_temp;
+        //第一個
+        private TextView tvDay;
+        private TextView tvDate;
+        private TextView tvLocation;
+        private TextView tvTemp;
+        private TextView tvLowtemp;
+        private TextView tvHighttemp;
+        private ImageView imageView;
 
         public ViewHolder(View itemView,int viewType) {
             super(itemView);
-            //if(viewType==0){
-
-           // }
+            if(viewType==0){
+                tv_temp=(TextView)itemView.findViewById(R.id.tv_temp);
+            }
+            if(viewType==1){
+                tvDay = (TextView) itemView.findViewById(R.id.tv_day);
+                tvDate = (TextView) itemView.findViewById(R.id.tv_date);
+                tvLocation = (TextView) itemView.findViewById(R.id.tv_location);
+                tvTemp = (TextView) itemView.findViewById(R.id.tv_temp);
+                tvLowtemp = (TextView) itemView.findViewById(R.id.tv_lowtemp);
+                tvHighttemp = (TextView) itemView.findViewById(R.id.tv_highttemp);
+                imageView = (ImageView) itemView.findViewById(R.id.imageView);
+           }
         }
     }
 
-    private class HeaderHolder extends RecyclerView.ViewHolder {
-        public TextView tv_temp;
+     class HeaderHolder extends RecyclerView.ViewHolder{
+
         public HeaderHolder(View itemView) {
             super(itemView);
-            tv_temp=(TextView)itemView.findViewById(R.id.tv_temp);
-            tv_temp.setText(MainActivity.mTemp);
+
         }
     }
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        Toast.makeText(context,mPosition+" "+viewType+" ",Toast.LENGTH_SHORT).show();
+//        if (viewType == TYPE_HEADER) {
+//            return new HeaderHolder(LayoutInflater.from(context).inflate(R.layout.rv_header_weather_now, parent, false));
+//        }
+//         else if(viewType==1)
+//            return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item1, parent, false),viewType);
+//         else if(viewType==2)
+//            return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item2, parent, false),viewType);
+//        else if(viewType==9)
+//            return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item10, parent, false),viewType);
+//        else
+//            return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item_weather_now, parent, false),viewType);
+//    }
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Toast.makeText(context,mPosition+" "+viewType+" ",Toast.LENGTH_SHORT).show();
         if (viewType == TYPE_HEADER) {
-            return new HeaderHolder(LayoutInflater.from(context).inflate(R.layout.rv_header_weather_now, parent, false));
+            return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_header_weather_now, parent, false),viewType);
         }
          else if(viewType==1)
             return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.rv_item1, parent, false),viewType);
@@ -66,10 +99,42 @@ public class WeatherNowRVA extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
         mPosition=position;
-
+        if(mPosition==0)
+            viewHolder.tv_temp.setText(MainActivity.mTemp);
+        if(mPosition==1){
+            viewHolder.tvDate.setText(MainActivity.mDate]]]]]]]f                                                                                                                                                                                                                                                                                                                                                                                   ] ] ]     ]] ] ]]                                                                     );
+            viewHolder.tvDay.setText(MainActivity.mDay);
+            viewHolder.tvLocation.setText(MainActivity.mLocation);
+            viewHolder.tvTemp.setText(MainActivity.mTemp);
+            viewHolder.tvLowtemp.setText(MainActivity.mLowTemp);
+            viewHolder.tvHighttemp.setText(MainActivity.mHightTemp);
+            /*//Toast.makeText(MainActivity.this,myWeather.forecasttext0+" ",Toast.LENGTH_SHORT).show();
+            if(myWeather.forecastdate0==null)
+                Toast.makeText(MainActivity.this,"連接網路好不",Toast.LENGTH_SHORT).show();
+            else if(myWeather.forecasttext0.toString().equals("Partly Cloudy"))*/
+            //imageView.setImageResource(R.drawable.c);
+        }
     }
+
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+//        mPosition=position;
+//        if(mPosition==1){
+//            viewHolder.
+//            tvDate.setText(myWeather.forecastdate0);
+//            tvLocation.setText(myWeather.city);
+//            tvTemp.setText(myWeather.conditiontemp+" °C");
+//            tvLowtemp.setText(myWeather.forecastlow0 +" °C");
+//            tvHighttemp.setText(myWeather.forecasthigh0+" °C");
+//            Toast.makeText(MainActivity.this,myWeather.forecasttext0+" ",Toast.LENGTH_SHORT).show();
+//            if(myWeather.forecastdate0==null)
+//                Toast.makeText(MainActivity.this,"連接網路好不",Toast.LENGTH_SHORT).show();
+//            else if(myWeather.forecasttext0.toString().equals("Partly Cloudy"))
+//            //imageView.setImageResource(R.drawable.c);
+//        }
+//    }
 
     @Override
     public int getItemCount() {
